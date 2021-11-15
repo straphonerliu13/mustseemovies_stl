d50d280012e21f36714a195fb1df1cef24bde146
anonymous anonymous <s.traphonerliu@gmail.com>
2021-11-15 06:11:29 +0000

    Direct association between UserResource and BookmarkResource

--- app/resources/bookmark_resource.rb
+++ app/resources/bookmark_resource.rb
@@ -7,6 +7,8 @@ class BookmarkResource < ApplicationResource
 
   # Direct associations
 
+  belongs_to :user
+
   # Indirect associations
 
 end
--- app/resources/user_resource.rb
+++ app/resources/user_resource.rb
@@ -9,6 +9,8 @@ class UserResource < ApplicationResource
 
   # Direct associations
 
+  has_many   :bookmarks
+
   has_many   :reviews
 
   # Indirect associations
