907440f2ead3ec76de4547a02e4ed65c4ae4cc30
anonymous anonymous <s.traphonerliu@gmail.com>
2021-11-15 06:11:23 +0000

    Generate graphiti resources for movies

---
+++ app/resources/movie_resource.rb
@@ -0,0 +1,15 @@
+class MovieResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :year, :integer
+  attribute :duration, :integer
+  attribute :description, :string
+  attribute :director_id, :integer
+  attribute :actor_id, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end
