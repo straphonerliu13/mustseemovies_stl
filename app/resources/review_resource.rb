f20e34d5b83a101f90c8a850104024b315108873
anonymous anonymous <s.traphonerliu@gmail.com>
2021-11-15 06:11:29 +0000

    Direct association between UserResource and ReviewResource

--- app/resources/review_resource.rb
+++ app/resources/review_resource.rb
@@ -9,6 +9,8 @@ class ReviewResource < ApplicationResource
 
   # Direct associations
 
+  belongs_to :user
+
   # Indirect associations
 
 end
--- app/resources/user_resource.rb
+++ app/resources/user_resource.rb
@@ -9,6 +9,8 @@ class UserResource < ApplicationResource
 
   # Direct associations
 
+  has_many   :reviews
+
   # Indirect associations
 
 end
